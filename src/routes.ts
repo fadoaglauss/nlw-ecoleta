2939f2ec82fb46c9fea0c9169947f12feee8d665
Fadoa Glauss <fadoa.glauss@gmail.com>
2020-06-02 22:24:57 -0300

    Day 2: Listed a point.

--- src/routes.ts
+++ src/routes.ts
@@ -1,56 +1,15 @@
 import express, { request, response } from 'express';
-import knex from './database/connection'
+
+import PointsController from './controllers/PointsControlles';
+import ItemsController from './controllers/ItemsControllers';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController;
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3000/public/images/${item.image}`
-        }
-    });
-    return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        name,
-        image: 'image-fake',
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    });    
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id
-        }
-    });
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({ sucess: true })
-})
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
