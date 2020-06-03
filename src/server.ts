1351c500b21f69c2bf0ab5c79a1528c503449fbf
Fadoa Glauss <fadoa.glauss@gmail.com>
2020-06-02 22:00:11 -0300

    Day 1: Registered point.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes';
-
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/public/images', express.static(path.resolve(__dirname, '..', 'public', 'images')));
 
 app.listen(3000);
