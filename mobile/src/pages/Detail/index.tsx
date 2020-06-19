cea088cfb5e872b92412f9bd23d7d0173cb11c3c
Fadoa Glauss <fadoa.glauss@gmail.com>
2020-06-18 23:40:41 -0300

    Finish mobile app; Correct bug fixture

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { View, StyleSheet, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import Constants from 'expo-constants';
-import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import * as MailComposer from 'expo-mail-composer';
+
+import api from '../../services/api';
+
+interface Params {
+	point_id: number;
+}
+
+interface Data {
+	point: {
+		image: string;
+		name: string;
+		email: string;
+		whatsapp: string;
+		city: string;
+		uf: string;
+	};
+	items: {
+		title: string;
+	}[];
+}
 
 const Detail = () => {
+	const [data, setData] = useState<Data>({} as Data);
+
 	const navigation = useNavigation();
+
+	const route = useRoute();
+	const routeParams = route.params as Params;
+
+	useEffect(() => {
+		api.get(`points/${routeParams.point_id}`).then(response => {
+			setData(response.data);
+		});
+	}, []);
 
 	function handleNavigateBack() {
 		navigation.goBack();
+	}
+
+	function handleComposeWhatsapp() {
+		Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos.`);
+	}
+
+	function handleComposeMail() {
+		MailComposer.composeAsync({
+			subject: 'Interesse na coleta de resíduos',
+			recipients: [data.point.email],
+		});
+	}
+
+	if (!data.point) {
+		return (
+			<>
+			</>
+		);
 	}
 
 	return (
@@ -18,21 +68,21 @@ const Detail = () => {
 				<TouchableOpacity onPress={handleNavigateBack}>
 					<Icon name="arrow-left" size={20} color="#34cb79" />
 				</TouchableOpacity>
-				<Image style={styles.pointImage} source={{ uri: 'https://blog.guiabolso.com.br/wp-content/uploads/2018/02/mercado-1-1-700x525.jpg' }} />
-				<Text style={styles.pointName}>Mercadão</Text>
-				<Text style={styles.pointItems}>Lŝmpadas</Text>
+				<Image style={styles.pointImage} source={{ uri: data.point.image }} />
+				<Text style={styles.pointName}>{data.point.name}</Text>
+				<Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 				<View style={styles.address}>
 					<Text style={styles.addressTitle}>Endereço</Text>
-					<Text style={styles.addressContent}>Belo Horizonte, MG</Text>
+					<Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
 
 				</View>
 			</View>
 			<View style={styles.footer}>
-				<RectButton style={styles.button} onPress={() => { }}>
+				<RectButton style={styles.button} onPress={handleComposeWhatsapp}>
 					<FontAwesome name="whatsapp" size={20} color="#FFF" />
 					<Text style={styles.buttonText}>Whatsapp</Text>
 				</RectButton>
-				<RectButton style={styles.button} onPress={() => { }}>
+				<RectButton style={styles.button} onPress={handleComposeMail}>
 					<Icon name="mail" size={20} color="#FFF" />
 					<Text style={styles.buttonText}>E-mail</Text>
 				</RectButton>
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,14 +1,71 @@
-import React from 'react';
-import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ImageBackground, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler'
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import RNPickerSelect from 'react-native-picker-select';
+
+import axios from 'axios';
+
+interface IBGEUFResponse {
+	sigla: string;
+}
+
+interface IBGECityResponse {
+	nome: string;
+}
+
+interface UF {
+	label: string;
+	value: string;
+}
+
+interface City {
+	label: string;
+	value: string;
+}
 
 const Home = () => {
 	const navigation = useNavigation();
 
+	const [UFs, setUFs] = useState<UF[]>([]);
+	const [cities, setCities] = useState<City[]>([]);
+
+	const [selectedUF, setSelectedUF] = useState('0');
+	const [selectedCity, setSelectedCity] = useState('0');
+
+	useEffect(() => {
+		axios
+			.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+			.then(response => {
+				const ufInitials = response.data.map(uf => { return { label: uf.sigla, value: uf.sigla } });
+				setUFs(ufInitials);
+			});
+	}, []);
+
+	useEffect(() => {
+		if (selectedUF === '0') {
+			return;
+		}
+
+		axios
+			.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUF}/municipios`)
+			.then(response => {
+				const cityNames = response.data.map(city => { return { label: city.nome, value: city.nome } });
+				setCities(cityNames);
+			});
+	}, [selectedUF]);
+
 	function handleNavigateToPoints() {
-		navigation.navigate("Points");
+		navigation.navigate("Points", { uf: selectedUF, city: selectedCity });
+	}
+
+	function handleSelectUF(uf: string) {
+		setSelectedUF(uf);
+	}
+
+	function handleSelectCity(city: string) {
+		setSelectedCity(city);
 	}
 
 	return (
@@ -30,6 +87,42 @@ const Home = () => {
 			</View>
 
 			<View style={styles.footer}>
+				<RNPickerSelect
+					placeholder={{
+						label: 'Selecione um UF',
+						value: null,
+						color: '#FFF',
+					}}
+					items={UFs}
+					onValueChange={value => handleSelectUF(value)}
+					style={{
+						...styles,
+						iconContainer: {
+							top: 20,
+							right: 15,
+						},
+					}}
+					value={selectedUF}
+					useNativeAndroidPickerStyle={false}
+				/>
+				<RNPickerSelect
+					placeholder={{
+						label: 'Selecione uma cidade',
+						value: null,
+						color: '#FFF',
+					}}
+					items={cities}
+					onValueChange={value => handleSelectCity(value)}
+					style={{
+						...styles,
+						iconContainer: {
+							top: 20,
+							right: 15,
+						},
+					}}
+					value={selectedCity}
+					useNativeAndroidPickerStyle={false}
+				/>
 				<RectButton style={styles.button} onPress={handleNavigateToPoints}>
 					<View style={styles.buttonIcon}>
 						<Text>
@@ -38,7 +131,7 @@ const Home = () => {
 					</View>
 					<Text style={styles.buttonText}>
 						Entrar
-          </Text>
+          			</Text>
 				</RectButton>
 			</View>
 		</ImageBackground>
@@ -79,6 +172,24 @@ const styles = StyleSheet.create({
 	select: {},
 
 	input: {
+		height: 60,
+		backgroundColor: '#FFF',
+		borderRadius: 10,
+		marginBottom: 8,
+		paddingHorizontal: 24,
+		fontSize: 16,
+	},
+
+	inputIOS: {
+		height: 60,
+		backgroundColor: '#FFF',
+		borderRadius: 10,
+		marginBottom: 8,
+		paddingHorizontal: 24,
+		fontSize: 16,
+	},
+
+	inputAndroid: {
 		height: 60,
 		backgroundColor: '#FFF',
 		borderRadius: 10,
