530e28b368dbb2e9fd896dd30f6832983075fd5b
Fadoa Glauss <fadoa.glauss@gmail.com>
2020-06-19 23:47:01 -0300

    [mobile] Separate page styles files

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { View, StyleSheet, Text, Image, TouchableOpacity, Linking } from 'react-native';
-import Constants from 'expo-constants';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import * as MailComposer from 'expo-mail-composer';
+
+import styles from './styles';
 
 import api from '../../services/api';
 
@@ -15,6 +16,7 @@ interface Params {
 interface Data {
 	point: {
 		image: string;
+		image_url: string;
 		name: string;
 		email: string;
 		whatsapp: string;
@@ -68,7 +70,7 @@ const Detail = () => {
 				<TouchableOpacity onPress={handleNavigateBack}>
 					<Icon name="arrow-left" size={20} color="#34cb79" />
 				</TouchableOpacity>
-				<Image style={styles.pointImage} source={{ uri: data.point.image }} />
+				<Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
 				<Text style={styles.pointName}>{data.point.name}</Text>
 				<Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 				<View style={styles.address}>
@@ -90,79 +92,5 @@ const Detail = () => {
 		</>
 	);
 };
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 32,
-		paddingTop: 20 + Constants.statusBarHeight,
-	},
-
-	pointImage: {
-		width: '100%',
-		height: 120,
-		resizeMode: 'cover',
-		borderRadius: 10,
-		marginTop: 32,
-	},
-
-	pointName: {
-		color: '#322153',
-		fontSize: 28,
-		fontFamily: 'Ubuntu_700Bold',
-		marginTop: 24,
-	},
-
-	pointItems: {
-		fontFamily: 'Roboto_400Regular',
-		fontSize: 16,
-		lineHeight: 24,
-		marginTop: 8,
-		color: '#6C6C80'
-	},
-
-	address: {
-		marginTop: 32,
-	},
-
-	addressTitle: {
-		color: '#322153',
-		fontFamily: 'Roboto_500Medium',
-		fontSize: 16,
-	},
-
-	addressContent: {
-		fontFamily: 'Roboto_400Regular',
-		lineHeight: 24,
-		marginTop: 8,
-		color: '#6C6C80'
-	},
-
-	footer: {
-		borderTopWidth: StyleSheet.hairlineWidth,
-		borderColor: '#999',
-		paddingVertical: 20,
-		paddingHorizontal: 32,
-		flexDirection: 'row',
-		justifyContent: 'space-between'
-	},
-
-	button: {
-		width: '48%',
-		backgroundColor: '#34CB79',
-		borderRadius: 10,
-		height: 50,
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-
-	buttonText: {
-		marginLeft: 8,
-		color: '#FFF',
-		fontSize: 16,
-		fontFamily: 'Roboto_500Medium',
-	},
-});
 
 export default Detail;
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, ImageBackground, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, ImageBackground, Text, Image} from 'react-native';
 import { RectButton } from 'react-native-gesture-handler'
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import RNPickerSelect from 'react-native-picker-select';
+
+import styles from './styles';
 
 import axios from 'axios';
 
@@ -137,93 +139,5 @@ const Home = () => {
 		</ImageBackground>
 	)
 };
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 32,
-		backgroundColor: '#f0f0f5',
-	},
-
-	main: {
-		flex: 1,
-		justifyContent: 'center',
-	},
-
-	title: {
-		color: '#322153',
-		fontSize: 32,
-		fontFamily: 'Ubuntu_700Bold',
-		maxWidth: 260,
-		marginTop: 64,
-	},
-
-	description: {
-		color: '#6C6C80',
-		fontSize: 16,
-		marginTop: 16,
-		fontFamily: 'Roboto_400Regular',
-		maxWidth: 260,
-		lineHeight: 24,
-	},
-
-	footer: {},
-
-	select: {},
-
-	input: {
-		height: 60,
-		backgroundColor: '#FFF',
-		borderRadius: 10,
-		marginBottom: 8,
-		paddingHorizontal: 24,
-		fontSize: 16,
-	},
-
-	inputIOS: {
-		height: 60,
-		backgroundColor: '#FFF',
-		borderRadius: 10,
-		marginBottom: 8,
-		paddingHorizontal: 24,
-		fontSize: 16,
-	},
-
-	inputAndroid: {
-		height: 60,
-		backgroundColor: '#FFF',
-		borderRadius: 10,
-		marginBottom: 8,
-		paddingHorizontal: 24,
-		fontSize: 16,
-	},
-
-	button: {
-		backgroundColor: '#34CB79',
-		height: 60,
-		flexDirection: 'row',
-		borderRadius: 10,
-		overflow: 'hidden',
-		alignItems: 'center',
-		marginTop: 8,
-	},
-
-	buttonIcon: {
-		height: 60,
-		width: 60,
-		backgroundColor: 'rgba(0, 0, 0, 0.1)',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-
-	buttonText: {
-		flex: 1,
-		justifyContent: 'center',
-		textAlign: 'center',
-		color: '#FFF',
-		fontFamily: 'Roboto_500Medium',
-		fontSize: 16,
-	}
-});
 
 export default Home;
