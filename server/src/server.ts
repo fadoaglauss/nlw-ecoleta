bb52ed82f83e8a043a059d3930774a7d49d0ecda
Fadoa Glauss <fadoa.glauss@gmail.com>
2020-06-20 12:11:50 -0300

    Finish nlw!

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
 import cors from 'cors';
-import routes from './routes';
 import path from 'path';
+import express from 'express';
+import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -12,5 +13,7 @@ app.use(routes);
 
 app.use('/img/page', express.static(path.resolve(__dirname, '..', 'img', 'page')));
 app.use('/img/gallery', express.static(path.resolve(__dirname, '..', 'img', 'gallery')));
+
+app.use(errors ());
 
 app.listen(3000);
